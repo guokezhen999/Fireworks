e811b6f5401fcc80769ef66267de14f5422c1977
郭珂桢 <2216517282@qq.com>
2025-02-13 15:37:06 +0800

    Initial Commit

---
+++ Fireworks/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Fireworks
+//
+//  Created by 郭珂桢 on 2025/2/13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
